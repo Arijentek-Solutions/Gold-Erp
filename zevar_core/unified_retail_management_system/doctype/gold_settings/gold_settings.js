d94ad81b8e57b88695226f710cadc39f1bc1b105
Akshayaritek <akshay@arijentek.com>
2025-12-23 08:50:27 +0000

    feat: add gold settings, metal type, purity doctypes

---
+++ zevar_core/unified_retail_management_system/doctype/gold_settings/gold_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Arijentek Solutions and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Gold Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
