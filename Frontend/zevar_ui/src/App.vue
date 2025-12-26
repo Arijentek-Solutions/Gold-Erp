28b8db92fc4675ac459a6d3471bc48e8123d826f
Akshayaritek <akshay@arijentek.com>
2025-12-26 11:33:21 +0000

    chore: Remove duplicate Frontend directory

--- Frontend/zevar_ui/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="h-screen w-screen bg-gray-50">
-    <router-view />
-  </div>
-</template>
-
-<script setup>
-// The router will determine if we show Login or POS
-</script>
